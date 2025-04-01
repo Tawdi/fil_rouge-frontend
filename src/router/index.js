52fa6a5905912411588935a8b8cf577b40d5bdee
Tawdi <ahmedtawdi00@gmail.com>
2025-04-01 20:35:37 +0000

    ajouter routes

--- src/router/index.js
+++ src/router/index.js
@@ -4,14 +4,30 @@ import Movies from '../views/Movies.vue'
 import Cinemas from '../views/Cinemas.vue'
 import Support from '../views/Support.vue'
 import About from '../views/About.vue'
+// ===================== cinema admin ================================
+import CinemaAdminDashboard from '../views/cinema_admin/Dashboard.vue' 
+// =====================  admin ================================
+import AdminDashboard from '../views/admin/Dashboard.vue'  
 const router = createRouter({
   history: createWebHistory(), 
   routes: [
+    // ===============================================================
+    // =========================  user  ==============================
+    // ===============================================================
     { path: '/', component: Home },
     { path: '/movies', component: Movies },
     { path: '/cinemas', component: Cinemas },
     { path: '/support', component: Support },
     { path: '/about', component: About },
+    // ===============================================================
+    // =========================  cinema admin =======================
+    // ===============================================================
+    { path: '/admin/cinema', component: CinemaAdminDashboard },
+
+    // ===============================================================
+    // =========================  super admin ========================
+    // ===============================================================
+    { path: '/admin/', component: AdminDashboard },
   ],
 });
 
