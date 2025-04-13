e63a1bc769ff688f6e64143c1b8023bdd07e58a5
Tawdi <ahmedtawdi00@gmail.com>
2025-04-13 01:08:07 +0100

    Header.vue | fix error pour model de search

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -95,8 +95,8 @@
                 </li>
               </ul>
           </nav>
-          <div class=" items-center gap-4 flex justify-center" @click="toggleSearchModal">
-            <button class="text-[#999999] hover:text-white p-2">
+          <div class=" items-center gap-4 flex justify-center">
+            <button class="text-[#999999] hover:text-white p-2"  @click="toggleSearchModal">
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search"><circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/></svg>
             </button>
 
