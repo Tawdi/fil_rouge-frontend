10ae88753f9bed777bf364a4f3418c55ebb63bde
Tawdi <ahmedtawdi00@gmail.com>
2025-04-16 00:26:34 +0100

    adminDashboard | test stats card fake data

--- src/views/admin/Dashboard.vue
+++ src/views/admin/Dashboard.vue
@@ -3,6 +3,7 @@
         <main class="flex-1 overflow-y-auto p-6">
             <DashboardHeader />
 
+            <StatsCards :stats="stats" />
         </main>
     </div>
 
@@ -10,6 +11,25 @@
 
 <script setup>
 import DashboardHeader from '../../components/admin/DashboardHeader.vue'
+import StatsCards from '../../components/admin/StatsCards.vue'
+
+
+const stats = [
+    {
+        name: 'Total Revenue',
+        value: '$24,567',
+    },
+    {
+        name: 'Bookings',
+        value: '1,234',
+    },
+    {
+        name: 'Active Users',
+        value: '5,678',
+    },
+
+];
+
 </script>
 
 <style></style>
