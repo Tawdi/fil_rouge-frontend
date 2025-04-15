96030413e64b4c743ba976c0c8fce2085d5bcd46
Tawdi <ahmedtawdi00@gmail.com>
2025-04-15 14:41:54 +0100

    Refactorisation | ajoute autre layouts pour admin et cinema-admin

---
+++ src/layouts/AdminLayout.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex">
+      <AdminSidebar />
+      <main class="ml-64 w-full p-6">
+        <router-view />
+      </main>
+    </div>
+  </template>
+  
+  <script setup>
+  import AdminSidebar from '../components/admin/Sidebar.vue'
+  </script>
