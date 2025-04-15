e3a672142a49b41c2d8504b602969bb9384bced2
Tawdi <ahmedtawdi00@gmail.com>
2025-04-15 23:56:12 +0100

    ajoute dashbord header au dashboard.vue

---
+++ src/components/admin/DashboardHeader.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="mb-6">
+    <h1 class="text-2xl font-bold">Dashboard</h1>
+    <p class="text-[#999999]">Welcome back, Admin. Here's what's happening today.</p>
+  </div>
+</template>
--- src/views/admin/Dashboard.vue
+++ src/views/admin/Dashboard.vue
@@ -1,4 +1,15 @@
 <template>
-admin Dashboard page
+    <div class=" flex flex-col overflow-hidden">
+        <main class="flex-1 overflow-y-auto p-6">
+            <DashboardHeader />
+
+        </main>
+    </div>
+
 </template>
-<script></script>
+
+<script setup>
+import DashboardHeader from '../../components/admin/DashboardHeader.vue'
+</script>
+
+<style></style>
