501c17eb64b54416c78106ad08e5e5885d5e5538
Tawdi <ahmedtawdi00@gmail.com>
2025-04-15 14:40:27 +0100

    Refactorisation | ajoute de layout pour rendre App.vue utilise multi layout

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="min-h-screen bg-[#141414] text-white">
+      <!-- Header -->
+      <Header></Header>
+  
+      <main>
+        <router-view></router-view>
+      </main>
+  
+      <!-- Footer -->
+       <Footer></Footer>
+    </div>
+  </template>
+  
+  <script setup>
+  import Header from "../components/Header.vue";
+  import Footer from "../components/Footer.vue";
+  </script>
+  
+  <style>
+  .container,
+  .navbar {
+    width: 100%;
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 0.5rem 1rem;
+  }
+  
+  @media (min-width: 640px) {
+    .container,
+    .navbar {
+      padding: 1rem 1.5rem;
+    }
+  }
+  
+  @media (min-width: 1024px) {
+    .container.kk {
+      padding: 4rem 1rem;
+    }
+  
+    .navbar {
+      padding: 0.5rem 2rem;
+    }
+  }
+  </style>
+  
