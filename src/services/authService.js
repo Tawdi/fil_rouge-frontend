72e6f976a656c20614006942dd52c13c605ca1d3
Tawdi <ahmedtawdi00@gmail.com>
2025-04-19 19:52:27 +0100

    ajoute authservice pour separer  logique les request axios

---
+++ src/services/authService.js
@@ -0,0 +1,18 @@
+import axios from '@/utils/axios'
+
+export default {
+  async login(credentials) {
+    const { data } = await axios.post('/auth/login', credentials)
+    return data
+  },
+
+  async register(credentials) {
+    const { data } = await axios.post('/auth/register', credentials)
+    return data
+  },
+
+  async getUser() {
+    const responce = await axios.get('/auth/me')
+    return responce.data
+  }
+}
