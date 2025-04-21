aad00dc4b2b0fbec696c8ed934a30bf706932a11
Tawdi <ahmedtawdi00@gmail.com>
2025-04-21 12:46:36 +0100

    Admin Dashborad | modifier sidebar (ajoute) logout button

--- src/components/admin/Sidebar.vue
+++ src/components/admin/Sidebar.vue
@@ -5,7 +5,7 @@
     'fixed top-0 left-0 md:sticky md:top-0 md:block'
   ]">
     <!-- Sidebar content -->
-    <div class="h-full overflow-hidden" v-if="isOpen || isMobile">
+    <div class="h-full  flex flex-col overflow-hidden " v-if="isOpen || isMobile">
       <!-- Header -->
       <div class="flex items-center justify-between h-16 px-4 border-b border-[#333333]">
         <span class="ml-2 font-bold text-lg">MovieSeat</span>
@@ -21,7 +21,7 @@
       </div>
 
       <!-- Navigation -->
-      <nav class="py-4">
+      <nav class="py-4 flex-1 overflow-y-auto">
         <ul>
           <li v-for="item in navItems" :key="item.name">
             <RouterLink :to="item.route" :class="[
@@ -35,6 +35,17 @@
           </li>
         </ul>
       </nav>
+      <!-- Logout Button -->
+      <div class="px-4 py-4 border-t border-[#333333] text-[#999999] hover:bg-[#262626] hover:text-white cursor-pointer">
+        <div class="flex items-center" @click="auth.logout">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24"
+            stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round"
+              d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a2 2 0 01-2 2H6a2 2 0 01-2-2V7a2 2 0 012-2h5a2 2 0 012 2v1" />
+          </svg>
+          <span>Logout</span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -42,6 +53,9 @@
 <script setup>
 import { useRoute } from 'vue-router'
 import { computed } from 'vue'
+
+import { useAuthStore } from '@/stores/auth'
+const auth = useAuthStore()
 
 const props = defineProps({ isOpen: Boolean })
 defineEmits(['toggle-sidebar'])
