c4dbf929a0c5c49653e121e0299ade6c2dc8bba3
Tawdi <ahmedtawdi00@gmail.com>
2025-04-22 11:39:32 +0100

    CinemaAdmin | RoomManager | ajoute seat type section

---
+++ src/components/cinemaAdmin/roomManager/SeatLegend.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="mt-4 pt-4 border-t border-[#333333] flex flex-wrap gap-4">
+      <div class="flex items-center">
+        <div class="w-4 h-4 rounded bg-blue-500/20 border border-blue-500"></div>
+        <span class="ml-2 text-xs">Standard</span>
+      </div>
+      <div class="flex items-center">
+        <div class="w-4 h-4 rounded bg-purple-500/20 border border-purple-500"></div>
+        <span class="ml-2 text-xs">VIP</span>
+      </div>
+      <div class="flex items-center">
+        <div class="w-4 h-4 rounded bg-green-500/20 border border-green-500"></div>
+        <span class="ml-2 text-xs">Accessible</span>
+      </div>
+    </div>
+  </template>
