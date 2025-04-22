3405fa4178aa0485784bffd94f870eb072107536
Tawdi <ahmedtawdi00@gmail.com>
2025-04-22 11:02:25 +0100

    CinemaAdmin | RoomManager | creer section de gestion des rooms (header)

---
+++ src/views/cinema_admin/RoomManager.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex h-screen bg-[#141414] text-white">
+    <div class="flex-1 flex flex-col overflow-hidden">
+      <main class="flex-1 overflow-y-auto p-6">
+        <div class="mb-6 flex flex-col md:flex-row justify-between items-start md:items-center gap-4">
+          <div>
+            <h1 class="text-2xl font-bold">Room Manager</h1>
+            <p class="text-[#999999]">Create and customize cinema room layouts</p>
+          </div>
+          
+          
+        </div>
+
+      </main>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
